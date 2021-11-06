7d8f34a9ffbbec8ec8c81cd378f5d8e0446fdd58
YuriiNatykan <yurikdnuk3@gmail.com>
2021-11-06 20:17:09 +0200

    6.Redux

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -5,7 +5,7 @@ function SortPopup({ items }) {
   const [activeItem, setActiveItem] = React.useState(0);
   //сохранять дом элемент
   const sortRef = React.useRef();
-  const activeLabel = items[activeItem];
+  const activeLabel = items[activeItem].name;
 
   const onSelectItem = (index) => {
     setActiveItem(index);
@@ -49,12 +49,12 @@ function SortPopup({ items }) {
         <div className="sort__popup">
           <ul>
             {items &&
-              items.map((name, index) => (
+              items.map((obj, index) => (
                 <li
                   className={activeItem === index ? 'active' : ''}
                   onClick={() => onSelectItem(index)}
-                  key={`${name}_${index}`}>
-                  {name}
+                  key={`${obj.type}_${index}`}>
+                  {obj.name}
                 </li>
               ))}
           </ul>
